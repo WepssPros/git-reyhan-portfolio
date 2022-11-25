5c984ae7f2566dc44c6c4b48eb9200c165b65d6b
WepsPros <reyhandwiambrta@gmail.com>
2022-11-25 16:15:34 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,28 @@ const { Container } = require('postcss');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ['./*.{html,js}'],
   important: true,
   theme: {
     extend: {
       fontFamily: {
-        poppins : ['Poppins','sans-serif']
+        poppins: ['Poppins', 'sans-serif'],
       },
-      container : {
-        center : true,
+      container: {
+        center: true,
         padding: '1rem',
         screens: {
-          'sm' : "512px",
-          'md' : "720px",
-          'lg' : "1024px",
-          'xl' : "1140px",
-          '2xl' : "1320px"
-        }
+          sm: '512px',
+          md: '720px',
+          lg: '1024px',
+          xl: '1140px',
+          '2xl': '1320px',
+        },
       },
       backgroundImage: {
-        'hero-img': "url('/src/images/bg-image.png')"
-      }
+        'hero-img': "url('/src/images/bg-image.png')",
+      },
     },
   },
   plugins: [],
-}
+};
